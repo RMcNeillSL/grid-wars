1e71d2dbd1b8dae5fd967042004b35dd2d581adb
Matthew Webber <mwebber@scottlogic.co.uk>
2015-08-25 16:15:16 +0100

    2.01 - Skeleton rest service created

--- gridwars/src/main/java/com/majaro/gridwars/api/REST.java
+++ gridwars/src/main/java/com/majaro/gridwars/api/REST.java
@@ -1,5 +1,18 @@
 package com.majaro.gridwars.api;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Produces("application/json")
+@Path("/")
 public class REST {
 
+	@GET
+	@Path("/helloworld")
+	public Response getHelloWorld() {
+		String helloWorld = "Hello World!";
+		return Response.ok(helloWorld).build();
+	}
 }
