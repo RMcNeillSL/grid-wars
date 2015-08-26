db4f43de1a5c4d9a68ce90fd5cc433ffc0ef705e
Robert McNeill <rmcneill@scottlogic.com>
2015-08-26 16:08:53 +0100

    2.05 - Further tidy up of angular modulation

---
+++ gridwars-web/WebContent/app/partials/login/loginController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function () {
+	
+	function loginController () {
+	}
+	loginController.prototype = {
+			login: function () {
+				
+			}
+	}
+
+	
+	angular.module('gridWarsApp.login.module').controller('gridWarsApp.login.controller', loginController);
+}());
