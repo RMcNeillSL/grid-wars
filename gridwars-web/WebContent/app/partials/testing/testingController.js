523cfae9c35d8d8adafb8081f1c482eec29e8bd0
James Hill <james.hill@lcsltd.co.uk>
2015-08-27 14:48:42 +0100

    GW-23 - Test partial with some functionality for the benefit of rob

--- gridwars-web/WebContent/app/partials/testing/testingController.js
+++ gridwars-web/WebContent/app/partials/testing/testingController.js
@@ -2,14 +2,51 @@
 
 (function () {
 	
-	function testingController () {
-	}
-	testingController.prototype = {
-			login: function () {
+	function TestingController ($scope, testingService) {
+		
+		// Save injected items
+		this.$scope = $scope;
+		this.testingService = testingService;
+		
+		// Declare variables
+		$scope.username = "TestUser";
+		$scope.password = "password";
+		$scope.lobbyList = [];
+		
+		// Constructor functions
+		this.createAuthRequest = function(username, password, params) {
+			var newAuthRequest = {'usernameAttempt': username, 'passwordAttempt': password};
+			if (params) {
 				
+			}
+			return newAuthRequest;
+		}
+		
+	}
+	TestingController.prototype = {
+			login: function () {
+				this.testingService.sendLogin(this.createAuthRequest(this.$scope.username, this.$scope.password));
+			},
+			newLobby: function () {		
+				var self = this;
+				
+				var updateLobbies = function(newLobbyList) {
+					self.$scope.lobbyList = newLobbyList;
+					console.log(newLobbyList);
+				};
+				
+				var retrieveLobbies = function() {
+					self.testingService.getLobbies(updateLobbies);
+				}
+				
+				this.testingService.newLobby(retrieveLobbies);
+				
+			},
+			joinLobby: function () {
 			}
 	}
 
+	TestingController.$inject = ['$scope', 'gridWarsApp.testing.service'];
 	
-	angular.module('gridWarsApp.testing.module').controller('gridWarsApp.testing.controller', testingController);
+	angular.module('gridWarsApp.testing.module').controller('gridWarsApp.testing.controller', TestingController);
 }());
