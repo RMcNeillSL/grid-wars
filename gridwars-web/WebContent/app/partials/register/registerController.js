6d2b811905f1288dc948012f04b377d2cc557371
Robert McNeill <rmcneill@scottlogic.com>
2015-08-28 08:56:04 +0100

    GW-26 - Everything but error handling complete for client register page

--- gridwars-web/WebContent/app/partials/register/registerController.js
+++ gridwars-web/WebContent/app/partials/register/registerController.js
@@ -2,14 +2,37 @@
 
 (function () {
 	
-	function RegisterController () {
-	}
-	RegisterController.prototype = {
-			login: function () {
+	function RegisterController ($scope, registerService) {
+		// Save injected items
+		this.registerService = registerService;
+		this.$scope = $scope;
+		
+		// Declare variables
+		
+		// Constructor functions
+		this.createRegisterRequest = function (username, password, param) {
+			
+			var newRegisterRequest = {
+					"newUsername": username,
+					"newPassword": password
+			};
+			
+			if (param) {
 				
+			};
+			
+			return newRegisterRequest;
+		};
+	};
+	
+	RegisterController.prototype = {
+			register: function (username, password) {
+				var register = this.createRegisterRequest(username, password);
+				this.registerService.sendRegister(register);
 			}
 	}
 
+	RegisterController.$inject = ['$scope', 'gridWarsApp.register.service'];
 	
 	angular.module('gridWarsApp.register.module').controller('gridWarsApp.register.controller', RegisterController);
 }());
